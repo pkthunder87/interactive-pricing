2789a0936d4ab70e02eeaf38c9b2af08ba7a9673
pkthunder87 <pkthunder87@gmail.com>
2024-05-01 11:50:49 -0700

    Feat: Add tailwindcss to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div>Interactive Pricing Component</div>;
+  return (
+    <div className="text-3xl text-red-500">Interactive Pricing Component</div>
+  );
 }
 
 export default App;
