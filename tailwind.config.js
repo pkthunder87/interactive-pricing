09b3e164846f4eac2fb68d14a8522ce31893e961
pkthunder87 <pkthunder87@gmail.com>
2024-05-04 08:17:42 -0700

    Style: Add various colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,17 @@ export default {
     },
     extend: {
       colors: {
-        'primary-blue': 'hsl(246, 80%, 60%)',
+        'primary-soft-cyan': 'hsl(174, 77%, 80%)',
+        'primary-strong-cyan': 'hsl(174, 86%, 45%)',
+        'primary-light-grayish-red': 'hsl(14, 92%, 95%)',
+        'primary-light-red': 'hsl(15, 100%, 70%)',
+        'primary-pale-blue': 'hsl(226, 100%, 87%)',
+        'neutral-white': 'hsl (0, 0%, 100%)',
+        'neutral-very-pale-blue': 'hsl(230, 100%, 99%)',
+        'neutral-light-grayish-blue-slider': 'hsl(224, 65%, 95%)',
+        'neutral-light-graish-blue-toggle': 'hsl(223, 50%, 87%)',
+        'neutral-grayish-blue': 'hsl(225, 20%, 60%)',
+        'neutral-dark-desaturated-blue': 'hsl(227, 35%, 25%)',
       },
     },
   },
