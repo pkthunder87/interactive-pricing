f076772202005e7ab97698deeb5d4159017ab1e7
pkthunder87 <pkthunder87@gmail.com>
2024-05-11 10:08:08 -0700

    Feat: Add colors to PriceSlider track

--- src/components/PriceSlider.jsx
+++ src/components/PriceSlider.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function PriceSlider() {
   const [thumb, setThumb] = useState(50);
 
-  function onSlide(data) {
-    setThumb(() => data.target.value);
+  function handleSlide(data) {
+    setThumb(data.target.value);
   }
+
+  useEffect(() => {
+    // From https://stackoverflow.com/questions/18389224/how-to-style-html5-range-input-to-have-different-color-before-and-after-slider User dargue3
+
+    document.getElementById('slider').onchange = function () {
+      var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+      this.style.background =
+        'linear-gradient(to right, hsl(174, 77%, 80%) 0%,hsl(174, 86%, 45%), hsl(174, 77%, 80%) ' +
+        value +
+        '%, hsl(224, 65%, 95%) ' +
+        value +
+        '%, hsl(224, 65%, 95%) 100%)';
+    };
+  }, []);
 
   return (
     <div className="h-full w-[50dvw] rounded-md bg-neutral-white p-10 shadow-lg">
       <p>Pricing Component</p>
       <div className="flex justify-center">
         <input
-          className="slider "
+          className="slider h-2"
           type="range"
           min={1}
           max={100}
           value={thumb}
-          onChange={onSlide}
+          onChange={handleSlide}
           id="slider"
         />
       </div>
