1b8b4ffacf4c4bafcacd413c34ec58030e69a1b3
pkthunder87 <pkthunder87@gmail.com>
2024-05-12 08:57:12 -0700

    Refactor: Remove unnecessary code in PriceSlider useEffect

--- src/components/PriceSlider.jsx
+++ src/components/PriceSlider.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 function PriceSlider() {
   const [thumb, setThumb] = useState(50);
-  const [initRender, setInitRender] = useState(true);
 
   function handleSlide(data) {
     setThumb(data.target.value);
@@ -12,14 +11,12 @@ function PriceSlider() {
     // From https://stackoverflow.com/questions/18389224/how-to-style-html5-range-input-to-have-different-color-before-and-after-slider User dargue3
 
     document.getElementById('slider').onchange = function () {
-      var value = ((this.value - this.min) / (this.max - this.min)) * 100;
       this.style.background =
         'linear-gradient(to right, hsl(174, 77%, 80%) 0%,hsl(174, 86%, 45%), hsl(174, 77%, 80%) ' +
-        value +
+        this.value +
         '%, hsl(224, 65%, 95%) ' +
-        value +
-        '%, hsl(224, 65%, 95%) 100%)';
-      setInitRender(false);
+        this.value +
+        '%)';
     };
   }, []);
 
@@ -28,7 +25,7 @@ function PriceSlider() {
       <p>Pricing Component</p>
       <div className="flex justify-center">
         <input
-          className={`slider h-2 ${initRender ? 'track' : ''}`}
+          className={`slider track h-2`}
           type="range"
           min={1}
           max={100}
