6ecd342e8265513e3b28332dafde33eeda54dabe
pkthunder87 <pkthunder87@gmail.com>
2024-05-20 03:39:30 -0700

    Docs: Add a note about circle svg stroke/thickness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 function Header() {
   return (
+    // can't adjust svg background circle thickness/stroke
     <div
       className=" flex flex-col justify-center
     bg-svg-header bg-[length:140px_140px] bg-center bg-no-repeat text-center xl3:bg-[length:145px_145px] xl3:bg-[top_3.8rem_right_18rem]"
