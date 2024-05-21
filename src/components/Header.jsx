510eb16903d6305d91f1240fac386a9f8bf979fc
pkthunder87 <pkthunder87@gmail.com>
2024-05-21 16:49:01 -0700

    Feat: Style type half of PriceSlider for desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     // can't adjust svg background circle thickness/stroke
     <div
       className=" flex flex-col justify-center
-    bg-svg-header bg-[length:140px_140px] bg-center bg-no-repeat text-center xl3:bg-[length:145px_145px] xl3:bg-[top_3.8rem_right_18rem]"
+    bg-svg-header bg-[length:140px_140px] bg-center bg-no-repeat text-center xl3:bg-[length:145px_145px] xl3:bg-[top_3.8rem_right_12.5rem]"
     >
       <h1 className="text-2xl font-extrabold text-neutral-dark-desaturated-blue xl3:-mt-[.7rem] xl3:text-[1.76rem] xl3:leading-[1.4em]">
         Simple, traffic-based pricing
--- src/components/PriceSlider.jsx
+++ src/components/PriceSlider.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 function PriceSlider() {
-  const [thumb, setThumb] = useState(50);
+  const [thumb, setThumb] = useState(51);
 
   function handleSlide(data) {
     setThumb(data.target.value);
@@ -12,7 +12,7 @@ function PriceSlider() {
 
     document.getElementById('slider').onchange = function () {
       this.style.background =
-        'linear-gradient(to right, hsl(174, 77%, 80%) 0%,hsl(174, 86%, 45%), hsl(174, 77%, 80%) ' +
+        'linear-gradient(to right, hsl(174, 77%, 80%) 0%,hsl(174, 86%, 45%), ' +
         this.value +
         '%, hsl(224, 65%, 95%) ' +
         this.value +
@@ -21,18 +21,21 @@ function PriceSlider() {
   }, []);
 
   return (
-    <div className="h-full w-[50dvw] rounded-md bg-neutral-white p-10 shadow-lg xl3:-mt-5">
-      <div className="grid grid-cols-2 items-center gap-8">
-        <p className="text-[.6rem] font-extrabold tracking-widest">
+    <div className="h-full w-[50dvw] rounded-md bg-neutral-white p-10 shadow-lg xl3:-mt-5 xl3:w-[37.58dvw] xl3:rounded-[.6rem] xl3:py-11 xl3:pl-11 xl3:pr-[3.16rem]">
+      <div className="grid grid-cols-2 items-center gap-8  xl3:mt-[.4rem]">
+        <p className=" text-[.6rem] font-extrabold tracking-[.12em] xl3:text-[.9rem]">
           100K PAGEVIEWS
         </p>
         <div className="flex items-center justify-self-end">
-          <h2 className="text-3xl font-extrabold text-neutral-dark-desaturated-blue">
+          <h2 className="text-3xl font-extrabold text-neutral-dark-desaturated-blue  xl3:text-[2.5rem] xl3:tracking-[-.02em]">
             $16.00
           </h2>
-          <p className="ml-2 mt-1 text-[.6rem] font-semibold"> / month</p>
+          <p className="ml-2 mt-1 text-[.6rem] font-semibold xl3:mt-0 xl3:text-base">
+            {' '}
+            / month
+          </p>
         </div>
-        <div className="col-span-2 mt-1 flex justify-center">
+        <div className="col-span-2 mt-1 flex justify-center xl3:mt-[1.1rem]">
           <input
             className={`slider track h-2 `}
             type="range"
@@ -44,21 +47,25 @@ function PriceSlider() {
           />
         </div>
       </div>
-      <div className="mr-3 mt-12 flex items-center justify-end text-[.58rem] font-semibold">
+      <div className="mr-3 mt-12 flex items-center justify-end text-[.58rem] font-semibold xl3:mt-[3.4rem] xl3:text-xs">
         {/* From https://flowbite.com/docs/forms/toggle/ 
         Adjust after:start, after:top after:h to change toggle circle size
         */}
         <label className="inline-flex items-center">
-          <p className="mr-4">Monthly Billing</p>
+          <p className="mr-[1.1rem]">Monthly Billing</p>
           <input type="checkbox" value="" className="peer sr-only" />
-          <div className="after:border-neutral-light-grayish peer relative h-[1.15rem] w-9 cursor-pointer rounded-full bg-neutral-light-grayish-blue-toggle after:absolute after:start-[2px] after:top-[2px] after:h-[.9rem] after:w-[.9rem] after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-strong-cyan peer-checked:after:translate-x-full"></div>
+          <div
+            className="after:border-neutral-light-grayish peer relative h-[1.15rem] w-9 cursor-pointer rounded-full bg-neutral-light-grayish-blue-toggle after:absolute 
+          after:start-[2px] after:top-[2px] after:h-[.9rem] after:w-[.9rem]
+          after:rounded-full after:border after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary-strong-cyan peer-checked:after:translate-x-full xl3:mr-[.1rem] xl3:h-[1.35rem] xl3:w-[2.6rem] after:xl3:start-[4px] after:xl3:top-[3px] after:xl3:h-[.95rem] after:xl3:w-[.95rem]"
+          ></div>
         </label>
         <p className="ml-4">Yearly Billing</p>
-        <div className="ml-2 rounded-full bg-primary-light-grayish-red px-[.3rem] py-[.2rem] font-extrabold text-primary-light-red">
+        <div className="ml-2 rounded-full bg-primary-light-grayish-red px-[.3rem] py-[.2rem] font-extrabold text-primary-light-red xl3:mr-[.3rem] xl3:px-[.4rem] xl3:py-[.1rem] xl3:text-[.62rem]">
           25% discount
         </div>
       </div>
-      <hr className="-mx-10 mt-8" />
+      <hr className="-mx-10 mt-8 xl3:-ml-11 xl3:-mr-[3.16rem] xl3:mt-[2.6rem] " />
       <div className="grid h-[44%] grid-cols-[55%_45%] items-center">
         <div className="flex flex-col gap-2 text-[.6rem] font-semibold">
           <div className="flex items-center gap-3 ">
