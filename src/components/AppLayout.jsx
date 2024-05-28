3d3d8004c946f24c0e6b67a0e6f5280ec61bcb87
pkthunder87 <pkthunder87@gmail.com>
2024-05-27 19:53:26 -0700

    Style: Finish header and svg background for mobile

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import PriceSlider from './PriceSlider';
 
 function AppLayout() {
   return (
-    <div className="grid w-[100dvw] grid-rows-[30%_70%] justify-center sm:h-[100dvh] sm:grid-rows-[35%_50%_15%]   ">
+    <div className=" grid w-[100dvw] grid-rows-[460px_1000px] justify-center md:h-[100dvh] md:grid-rows-[35%_50%_15%]   ">
       <Header />
       <PriceSlider />
       <div className=" "></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ function Header() {
   return (
     // can't adjust svg background circle thickness/stroke
     <div
-      className=" flex flex-col justify-center
-    bg-svg-header bg-[length:140px_140px] bg-center bg-no-repeat text-center xl3:bg-[length:145px_145px] xl3:bg-[top_3.8rem_right_12.5rem]"
+      className=" mt-4 flex flex-col
+    justify-center bg-svg-header bg-[length:270px_270px] bg-center
+    bg-no-repeat text-center md:mt-0 md:bg-[length:140px_140px] xl3:bg-[length:145px_145px] xl3:bg-[top_3.8rem_right_12.5rem]"
     >
-      <h1 className="text-2xl font-extrabold text-neutral-dark-desaturated-blue xl3:-mt-[.7rem] xl3:text-[1.76rem] xl3:leading-[1.4em]">
+      <h1 className="text-[2.5rem] font-extrabold text-neutral-dark-desaturated-blue md:mt-0 md:text-2xl xl3:-mt-[.7rem] xl3:text-[1.76rem] xl3:leading-[1.4em]">
         Simple, traffic-based pricing
       </h1>
-      <p className="mt-[.6rem] text-[.81rem] font-semibold xl3:text-[.95rem]">
-        Sign-up for our 30-day trial. No credit card required.
-      </p>
+      <div className="mt-3 justify-center text-[1.6rem] font-semibold md:mt-[.6rem] md:flex md:w-full md:gap-[.1rem] md:text-[.81rem] xl3:text-[.95rem]">
+        <p>Sign-up for our 30-day trial.</p>
+        <p className="mt-2 md:mt-0">No credit card required.</p>
+      </div>
     </div>
   );
 }
